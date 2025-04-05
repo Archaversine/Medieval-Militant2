cc1079a007be35e9d6aea81e2e44c4f2c7fc1ce9
Adam Brohl <abrohl@ur.rochester.edu>
2025-04-05 14:13:33 -0400

    Starter window application

---
+++ README.md
@@ -0,0 +1,13 @@
+# Medieval Militant
+
+Compile the project with:
+
+```bash 
+cabal build
+```
+
+Run (and compile) the project with:
+
+```bash 
+cabal run
+```
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,21 @@
 module Main where
 
+import Raylib.Core
+import Raylib.Util
+import Raylib.Util.Colors
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do 
+    let width  = 800 
+        height = 600
+        title  = "Medieval Militant"
+        fps    = 60
+    
+    initWindowUnmanaged width height title
+    setTargetFPS fps
+    
+    -- game loop
+    whileWindowOpen0 $ do 
+        beginDrawing
+        clearBackground black
+        endDrawing
