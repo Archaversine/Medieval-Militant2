49c84d8dfb534205e208b641416a4e68ec6e8bd6
Archaversine <adam.brohl.w@gmail.com>
2025-04-08 21:22:15 -0400

    Add comments to World.hs

--- src/Game/World.hs
+++ src/Game/World.hs
@@ -7,10 +7,15 @@ import Apecs
 
 import Linear
 
+-- List of components an entity can have
+-- NOTE: An entity does not have to have any of these components
 newtype Pos = Pos (V2 Float)
 newtype Vel = Vel (V2 Float)
 newtype Rot = Rot Float -- rotation (degrees because apparently nobody understand radians)
 newtype Renderer = Renderer (V2 Float -> Float -> IO ())
 
+-- Automatically generates the world type
+-- The exact implementation isn't relevant, it's essentially 
+-- a list of references to maps that map entity IDs to their corresponding components
 makeWorldAndComponents "World" [''Pos, ''Vel, ''Rot, ''Renderer]
 
