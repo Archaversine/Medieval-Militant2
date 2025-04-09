4c273c3151df1eab32d8b4aa119c4e1964e3d7da
Archaversine <adam.brohl.w@gmail.com>
2025-04-08 21:19:52 -0400

    Add comments to Tower.hs

--- src/Entity/Tower.hs
+++ src/Entity/Tower.hs
@@ -13,9 +13,14 @@ import Raylib.Core.Textures
 import Raylib.Util.Colors
 
 -- TODO: Eventually separate into separate functions for each tower type
+-- | Default renderer for towers
+--
+-- This function takes in a position and a rotation, and then renders 
+-- a default tower
 defaultTowerRenderer :: V2 Float -> Float -> IO ()
 defaultTowerRenderer pos rot = drawTextureEx image_Sprite_Tower_DefaultV2 pos rot 1 white
 
+-- | Create a default tower given position and velocity
 createDefaultTower :: V2 Float -> Game ()
 createDefaultTower p = newEntity_ (Pos p, Rot 0, Renderer defaultTowerRenderer)
 
