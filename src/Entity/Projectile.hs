63d92a86ac6c63bc7bebb065187e970aac825d4e
Archaversine <adam.brohl.w@gmail.com>
2025-04-08 21:18:53 -0400

    Add comments to Projectile.hs

--- src/Entity/Projectile.hs
+++ src/Entity/Projectile.hs
@@ -12,8 +12,13 @@ import Linear
 import Raylib.Core.Textures
 import Raylib.Util.Colors
 
+-- | Default renderer for projectiles
+--
+-- This function takes in a position and a rotation, and then renders 
+-- a default projectile
 defaultProjectileRenderer :: V2 Float -> Float -> IO ()
 defaultProjectileRenderer pos rot = drawTextureEx image_Sprite_Projectiles_ArrowDefault pos rot 1 white
 
+-- | Create a default projectile given position and velocity
 createDefaultProjectile :: V2 Float -> V2 Float -> Game ()
 createDefaultProjectile p dir = newEntity_ (Pos p, Vel dir, Rot (unangle dir * 180/pi + 90), Renderer defaultProjectileRenderer)
